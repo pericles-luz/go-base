b3eedc523a394953f48ceccc72e75b4caef4ab16
Péricles Luz <pericles.luz@gmail.com>
2023-03-30 11:13:03 -0300

    feat: canal para envio de whatsapp

--- pkg/messaging/messaging.go
+++ pkg/messaging/messaging.go
@@ -5,11 +5,14 @@ type Channels struct {
 	SMS chan map[string]string
 	// Channel for sending messages to the messaging adapter
 	Email chan map[string]interface{}
+	// Channel for sending messages to the messaging adapter
+	Whatsapp chan map[string]interface{}
 }
 
 func NewChannels() *Channels {
 	return &Channels{
-		SMS:   make(chan map[string]string, 100),
-		Email: make(chan map[string]interface{}, 100),
+		SMS:      make(chan map[string]string, 100),
+		Email:    make(chan map[string]interface{}, 100),
+		Whatsapp: make(chan map[string]interface{}, 100),
 	}
 }
