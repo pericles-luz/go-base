64835691e49643aed7d64aeb2f3e13d8eb5cc5e8
Pericles Luz <pericles.luz@gmail.com>
2023-04-28 19:34:10 -0300

    feat: inclui função que gerar hash de bytes

--- pkg/utils/general.go
+++ pkg/utils/general.go
@@ -18,6 +18,11 @@ func isTesting() bool {
 func Hash256(s string) string {
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(s)))
 }
+
+func Hash256FromBytes(b []byte) string {
+	return fmt.Sprintf("%x", sha256.Sum256(b))
+}
+
 func GetOnlyNumbers(str string) string {
 	var numbers string
 	for _, char := range str {
